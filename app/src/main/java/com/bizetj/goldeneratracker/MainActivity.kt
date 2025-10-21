1c9e558839c384be96147f8b572a7073fa85b1f4
BIZETjln <julien31.bizet@gmail.com>
2025-10-21 23:23:13 +0200

    feat: Change greeting

--- app/src/main/java/com/bizetj/goldeneratracker/MainActivity.kt
+++ app/src/main/java/com/bizetj/goldeneratracker/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GoldenEraTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
